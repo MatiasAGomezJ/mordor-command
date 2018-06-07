aa59140657a86aafe2af361a07c66bfeab7f6c5a
David <gelpiorama@gmail.com>
2018-06-07 13:19:57 +0200

    instrucciones para el examen y formato main

--- src/main/java/org/mvpigs/commandpattern/App.java
+++ src/main/java/org/mvpigs/commandpattern/App.java
@@ -32,6 +32,7 @@ import org.mvpigs.commandpattern.tratamientos.TratamientoPedidoPeligroso;
  * NO PUEDES MODIFICAR EL CODIGO DE LOS CASOS TEST 
  * 
  * La clase App.java contiene el MAIN y ha de funcionar.
+ * NO PUEDES MODIFICAR EL CODIGO DEL MAIN
  * 
  * - Crea un proyecto MAVEN que incluya tu nombre y apellidos.
  * - Realiza commits periódicamente mientras avanzas en el desarrollo de la aplicación.
@@ -51,20 +52,28 @@ public class App {
         Pedido pedido = new PedidoInternacional("Comarca", 10);
         TratamientoPedido tratamientoInt = new TratamientoPedidoInternacional(
                                                     (PedidoInternacional) pedido);
+        
         System.out.println(oficina.printarStatus(oficina.procesa(tratamientoInt), pedido));
 
         pedido = new PedidoInternacional("Mordor", 10);
         tratamientoInt = new TratamientoPedidoInternacional((PedidoInternacional) pedido);
+        
         System.out.println(oficina.printarStatus(oficina.procesa(tratamientoInt), pedido));
 
         pedido = new PedidoPeligrosoOrden("Cima de los vientos", 
                                           "no limpiarse las uñas con este puñal");
         TratamientoPedido peligroso = new TratamientoPedidoPeligroso((PedidoPeligroso) pedido);
+        
         System.out.println(oficina.printarStatus(oficina.procesa(peligroso), pedido));
 
         pedido = new PedidoPeligrosoOrden("Monte del destino", 
                                           "no ponerselo en el dedo");
         peligroso = new TratamientoPedidoPeligroso((PedidoPeligroso) pedido);
-        System.out.println(oficina.printarStatus(oficina.procesa(peligroso), pedido));   
+        
+        System.out.println(oficina.printarStatus(oficina.procesa(peligroso), pedido));
+        
+        /**
+         * Los pedidos multiples se completan en el ultimo de los casos test 
+         */
     }
 }
